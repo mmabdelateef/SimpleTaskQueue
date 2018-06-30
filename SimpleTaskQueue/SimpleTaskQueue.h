6accb3bf5c0962b5fa2aef5e0776ab559beca2a0
mabdellateef <mmabdelateef@gmail.com>
2018-06-30 11:36:51 +0200

    Initial Commit

---
+++ SimpleTaskQueue/SimpleTaskQueue.h
@@ -0,0 +1,19 @@
+//
+//  SimpleTaskQueue.h
+//  SimpleTaskQueue
+//
+//  Created by Mostafa Abdellateef on 6/30/18.
+//  Copyright © 2018 Mostafa Abdellateef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleTaskQueue.
+FOUNDATION_EXPORT double SimpleTaskQueueVersionNumber;
+
+//! Project version string for SimpleTaskQueue.
+FOUNDATION_EXPORT const unsigned char SimpleTaskQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleTaskQueue/PublicHeader.h>
+
+
